3c137d228001abea448ad2ea07aa9e5b8438e5a8
Trevor Nelson <teafiend9@gmail.com>
2017-07-11 16:29:00 -0700

    Add debug mode and improve global function evaluation

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
+# TODO Add "help" command
+
 INTEGRAL_ITERATIONS = 1000000
 
-def build_function(expression: str, arg_tuple: tuple):
-	def inner_function(*args):
-		# arg_tuple = (x, y)
-		# args = 3, 5
-		arg_mapping = {arg_tuple[i] : args[i] for i in range(len(arg_tuple))}
-		return eval(expression, globals(), arg_mapping)
-	return inner_function
+DEBUG_MODE = True
+
+class InvalidCommandError(Exception):
+	pass
+
+class InvalidOperationError(Exception):
+	pass
+
 	
 
 class Interpreter:
@@ -16,32 +19,51 @@ class Interpreter:
 		self.names = n;
 		self.done_interpreting = False
 	
+	def build_function(self, expression: str, arg_tuple: tuple):
+		def inner_function(*args):
+			# arg_tuple = (x, y)
+			# args = 3, 5
+			arg_mapping = {arg_tuple[i] : args[i] for i in range(len(arg_tuple))}
+			# TODO Potential name conflict between args and global names; fix it
+			arg_mapping.update(self.names)
+			print(arg_mapping)
+			return eval(expression, globals(), arg_mapping)
+		return inner_function
+
 	def evaluate(self, expression: str):
 		super_tokens = expression.split(' ', 1)	
-		# TODO: does not correctly evaluate expressions with spaces
+		# TODO does not correctly evaluate expressions with spaces
 		if len(super_tokens) == 1:
 			self.execute_command(super_tokens[0])
 		else:
 			self.execute_operation(super_tokens[0], super_tokens[1])
 	
 	def execute_command(self, command: str):
-		if command == 'quit' or command == 'exit':
+		# A command is a verb; it does something
+		if command == 'quit' or command == 'exit' or command == "q":
 			self.done_interpreting = True
 		elif command == 'vars' or command == 'variables':
+			if DEBUG_MODE: 
+				print(self.names)
 			for key, val in self.names.items():
 				print("{} = {}".format(key, val))
-		else:
+		elif command in self.names or command.split('(')[0] in self.names:
 			print(eval(command, globals(), self.names))
+		else:
+			raise InvalidCommandError()
 	
 	def execute_operation(self, verb, statement):
+		# An operation has a verb and a statement;
+		# it does something with the statement
 		if verb == 'let':
 			self.perform_assignment(statement)
 		elif verb == 'integrate':
 			value = self.integrate(statement)
 			# Ideally, truncate at 1/(square root of number of iterations)
+			# TODO Make sure that math is correct
 			print("{:.3f}".format(value))
 		else:
-			print("not an assignment")
+			raise InvalidOperationError()
 	
 	def integrate(self, statement):
 		tokens = statement.split()
@@ -54,6 +76,7 @@ class Interpreter:
 		delta = diff / INTEGRAL_ITERATIONS
 		mapping = {var: lower}
 		accum = 0
+		# TODO Animate this with some kind of loading bar
 		print("Integrating with delta-{} of {}...".format(var, delta))
 		while mapping[var] < upper:
 			accum += self.names[func](mapping[var]) * delta
@@ -70,7 +93,7 @@ class Interpreter:
 			tokens = super_tokens[0].rstrip(')').split('(')
 			name = tokens[0] # maybe not needed
 			args = tuple(arg.strip() for arg in tokens[1].split(','))
-			func = build_function(super_tokens[1], args)
+			func = self.build_function(super_tokens[1], args)
 			self.names[name] = func
 
 
@@ -78,8 +101,11 @@ def prompt(inter: Interpreter):
 	user_in = input("$ ")
 	try:
 		inter.evaluate(user_in)
-	except:
-		print("Invalid input")
+	except InvalidOperationError:
+		print("Invalid Operation")
+		prompt(inter)
+	except InvalidCommandError:
+		print("Invalid Command")
 		prompt(inter)
 	return inter.done_interpreting
 
